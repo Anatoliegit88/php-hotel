6867eff0c6743941cc2be66b650c805de80d6a38
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-11-27 23:53:23 +0100

    table

--- index.php
+++ index.php
@@ -1,10 +1,72 @@
-<?php
-
-
-for ($i=0; $i<=5; $i++ ) {
- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body style="background-color: darkgray;">
+  
+<div class="container text-center m-5">
+<table class="table">
+  <thead class="table-dark">
+  <tr>
+      
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+      <th scope="col">Distance</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+      <th scope="row">Hotel Belvedere</th>
+      <td>Hotel Belvedere</td>
+      <td>Si</td>
+      <td>4</td>
+      <td>10.4 km</td>
+      
+    </tr>
+    <th scope="row">Hotel Futuro</th>
+      <td>Hotel Futuro</td>
+      <td>Si</td>
+      <td>2</td>
+      <td>2 km</td>
+     
+    </tr>
+    <th scope="row">Hotel Rivamare</th>
+      <td>Hotel Rivamare</td>
+      <td>No</td>
+      <td>1</td>
+      <td>1 km</td>
+     
+    </tr>
+    <th scope="row">Hotel Milano</th>
+      <td>Hotel Milano</td>
+      <td>Si</td>
+      <td>2</td>
+      <td>50 km</td>
+     
+    </tr>
+    <th scope="row">Hotel Bellavista</th>
+      <td>Hotel Bellavista</td>
+      <td>No</td>
+      <td>5</td>
+      <td>5.5 km</td>
+    
+    </tr>
+  </tbody>
+</table>
+</div>
 
   
+  
+  <?php
+  
+for ($i=0; $i<=5; $i++ ) {
 }  
     $hotels = [
 
@@ -53,12 +115,18 @@ for ($i=0; $i<=5; $i++ ) {
       var_dump($addHotel);
       $i++;
     };
-    // echo $addHotels[0]["name"]
-
     ?>
    
 
+</body>
+</html>
+
+
+
+
+   
+
    
     
     
-   <!-- <?php echo ["$hotels"];?> -->
+  
