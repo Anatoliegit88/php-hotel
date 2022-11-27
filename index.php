5e5e51c8aabe99696d7d8ed48616f16e08944054
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-11-27 14:29:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+
+for ($i=0; $i<=5; $i++ ) {
+ 
+
+  
+}  
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    $i=0;
+    
+    while ($i < count($hotels)) {
+      $addHotel = $hotels [$i];
+      var_dump($addHotel);
+      $i++;
+    };
+    // echo $addHotels[0]["name"]
+
+    ?>
+   
+
+   
+    
+    
+   <!-- <?php echo ["$hotels"];?> -->
